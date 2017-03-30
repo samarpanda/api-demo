bf47a69e6623c77dfbe70893b1b9d62850675931
Samar Panda <er.samarpanda@gmail.com>
2017-03-30 07:33:26 +0530

    Mongoose connection error handled

--- server.js
+++ server.js
@@ -3,13 +3,36 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+// User native Promises
 mongoose.Promise = global.Promise;
-var conn = mongoose.connect('mongodb://localhost/'+'api-demo');
+
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose default connection error:' + err);
+})
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose default connection disconnected');
+})
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected');
+});
+
+process.on('SIGINT', function(){
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  })
+})
+
+mongoose.connect('mongodb://localhost/'+'api-demo')
+.then(() => {
+  console.log('Connected')
+})
+.catch((err) => {
+  console.log(err.message);
+});
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
@@ -19,5 +42,6 @@ router.get('/', function(req, res){
 
 app.use('/api', require('./app/controllers/ContactController'));
 
+var port = process.env.PORT || 8080;
 app.listen(port)
 console.log('Server started at ', port);
